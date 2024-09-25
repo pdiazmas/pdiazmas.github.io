15de8fcd81c7cb8fff15717ad8ae0eb84e6dea8c
Pau Diaz <user@MacBook-Pro-de-Pau.local>
2024-09-25 23:11:09 +0200

    Curriculum updated and cloned

---
+++ README.md
@@ -0,0 +1,21 @@
+# CV Virtual | ArturoCode
+
+#### Técnico Superior de Desarrollo de Aplicaciones Web y Sistemas y Redes
+
+### Es un currículum online en el que están estas secciones:
+
+- Breve información sobre mí
+- Idiomas
+- Proyectos Web (Destacados, Literarios y Freelance)
+- Experiencia laboral
+- Educación y cursos
+- Contacto
+
+##### Incluye Modo Claro/Oscuro, Navegación por secciones, Ir hacia el comienzo de la página y distintas animaciones.
+
+<table>
+    <tr>
+        <td><img src="images/cv_pc.png" /></td>
+        <td><img src="images/cv_movil.jpg" /></td>
+    </tr>
+</table>
---
+++ db/db.js
@@ -0,0 +1,232 @@
+export default {
+  languages: [
+    {
+      title: "Spanish",
+      skillName: "Español (Nativo)",
+      color: "2",
+      percentage: "100",
+    },
+    {
+      title: "English",
+      skillName: "Inglés (C1 - FCE)",
+      color: "1",
+      percentage: "85",
+    },
+  ],
+  projects: {
+    web: [
+      {
+        projectName: "ArturoCode | Desarrollador web",
+        image: "images/cv-portfoliocode.png",
+        summary: "Una página web / portfolio dedicada a mi trabajo, experiencia y formación",
+        preview: "https://arturocode.vercel.app/",
+        techStack: [
+          "React",
+          "JavaScript",
+          "CSS...",
+        ],
+      },
+      {
+        projectName: "Linktree (Arturo's Version)",
+        image: "images/cv-linkscode.png",
+        summary:
+          "Una One-Page sencilla y adaptada a mis redes y proyectos como desarrollador web.",
+        preview: "https://arturocode.github.io/links/",
+        techStack: [
+          "HTML",
+          "CSS",
+          "Bootstrap..."
+        ],
+      },
+      {
+        projectName: "Blog Tech ⚡ Super & Notion",
+        image: "images/cv-blogcode.png",
+        summary:
+          "Blog minimalista. También puede ser una web completa fácil de mantener para un usuario común.",
+        preview: "https://arturocodeblog.super.site/",
+        techStack: ["Notion", "Super", "CSS", "Markdown"],
+      },
+    ],
+    literature: [
+      {
+        projectName: "Portfolio literario",
+        image: "images/cv-portfoliowriter.png",
+        summary: "Portfolio literario para profesionales del sector.",
+        preview: "https://arturourbanos-portfolio.vercel.app/",
+        techStack: ["HTML", "CSS", "JavaScript", "jQuery..."],
+      },
+      {
+        projectName: "Title | Book Social",
+        image: "",
+        summary: "Próximamente.",
+        preview: "#",
+        techStack: [""],
+      },
+      {
+        projectName: "Landing Book Page",
+        image: "",
+        summary: "Próximamente.",
+        preview: "#",
+        techStack: [""],
+      }
+    ],
+    freelance: [
+      {
+        projectName: "Excursiones Íbice",
+        image: "images/cv-ibice.png",
+        summary:
+          "Trabajo Final del Grado Superior de Desarrollo de Aplicaciones Web.",
+        preview: "https://github.com/ArturoCode/excursiones-ibice",
+        techStack: [
+          "Node.js",
+          "JS",
+          "Express",
+          "HTML&CSS",
+          "MySQL...",
+        ],
+      },
+      {
+        projectName: "@tu-usuario | Social",
+        image: "images/cv-linkswriter.png",
+        summary: "One-Page personalizada para todo tipo de creadores de contenido.",
+        preview: "https://arturourbanos-links.vercel.app/",
+        techStack: [
+          "HTML",
+          "CSS",
+          "Bootstrap..."
+        ],
+      },
+      {
+        projectName: "Tu-Wiki",
+        image: "",
+        summary: "Próximamente.",
+        preview: "#",
+        techStack: [
+          "MongoDB",
+          "Express",
+          "Angular",
+          "node.js..."
+        ],
+      },
+    ],
+  },
+  experience: [
+    {
+      title: "Acevedo S.L.",
+      duration: "Sept. 2022 - actualidad",
+      subtitle: "Full Stack Developer",
+      details: [
+        "Desarrollo de aplicaciones web (frontend y backend) cloud native con MEAN stack. Herramientas de despliegue como Docker. Control de ramas, versiones y repositorios.",
+      ],
+      tags: [
+        "Angular",
+        "Node.js",
+        "Express",
+        "MongoDB",
+        "Docker",
+        "TypeScript",
+        "GitHub",
+        "Ubuntu",
+      ],
+    },
+    {
+      title: "AlcaláBC - Colegio de Registradores de España",
+      duration: "Jun. 2019 - Aug. 2019",
+      subtitle: "Técnico de soporte de TI",
+      details: [
+        "Soporte técnico de programas (Deposito digital D2, Legalia, Internet Explorer, Java) y el funcionamiento relacionado con el portal del Colegio de Registradores de España.",
+      ],
+      tags: [
+        "Windows",
+        "Java",
+        "Internet Explorer",
+        "Legalia",
+        "Deposito Digital D2",
+      ],
+    },
+    {
+      title: "Serconi S.L.",
+      duration: "Mar. 2019 - Jun. 2019",
+      subtitle: "Técnico informático. Contrato de prácticas",
+      details: [
+        "Servicios de consultoría informática. Soporte remoto. Resolver problemas informáticos a clientes, optimización de software, políticas de gestión de ficheros...",
+      ],
+      tags: [
+        "Windows",
+        "Ubuntu",
+        "Thunderbird",
+        "Backup",
+        "Office",
+        "Soporte Técnico",
+        "Programas",
+      ],
+    },
+  ],
+  education: [
+    {
+      title: "Desarrollo de Aplicaciones Web",
+      duration: "2020 - 2022",
+      subtitle: "IES Juan de la Cierva, Madrid",
+      details: [
+        "Desarrollo, implementación, diseño, instalación, despliegue y mantenimiento de aplicaciones web. Instalación de Sistemas en Red, bases de datos y entornos de desarrollo.",
+      ],
+      tags: [
+        "Windows",
+        "Ubuntu",
+        "HTML",
+        "CSS",
+        "LESS",
+        "SASS",
+        "Bootstrap",
+        "Java",
+        "JavaScript",
+        "SQL",
+        "PHP",
+        "Symfony",
+      ],
+    },
+    {
+      title: "Formación complementaria",
+      duration: "2020 - actualidad",
+      subtitle: "OpenWebinars, Platzi, Benowu, Crehana...",
+      details: [
+        "Cursos de informática, desarrollo web y multiplataforma. Puedes consultarlo en arturocode.github.io/cursos/",
+      ],
+      tags: [
+        "React",
+        "Python",
+        "APIs",
+        "Git",
+        "Postman",
+        "GitHub",
+        "jQuery",
+        "GitLab",
+        "Figma",
+        "Markdown",
+        "npm",
+        "Super & Notion",
+      ],
+    },
+    {
+      title: "Sistemas Microinformáticos y Redes",
+      duration: "2017 - 2019",
+      subtitle: "IES Juan de la Cierva, Madrid",
+      details: [
+        "Instalación y configuración de SO, programas, redes locales y servicios en red. Montaje y mantenimiento de equipos y seguridad informática.",
+      ],
+      tags: [
+        "Windows",
+        "Linux",
+        "Redes",
+        "Office Suite",
+        "Montaje",
+        "Seguridad informática",
+        "HTML",
+        "CSS",
+        "XML",
+        "Notepad++",
+        "Joomla",
+      ],
+    },
+  ],
+};
